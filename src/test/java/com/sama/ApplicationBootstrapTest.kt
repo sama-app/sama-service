38296be0b0fac305351862eaec34255241193d98
balysv <balys@valentukevicius.com>
2021-06-12 17:46:13 +0200

    Add testcontainers and a bootstrap test

---
+++ src/test/java/com/sama/ApplicationBootstrapTest.kt
@@ -0,0 +1,41 @@
+package com.sama
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ApplicationBootstrapTest(@Autowired val context: ApplicationContext) {
+
+    companion object {
+        @Container
+        val container: PostgreSQLContainer<*> = PostgreSQLContainer<Nothing>("postgres:13-alpine")
+            .apply {
+                withDatabaseName("sama-test")
+                withUsername("test")
+                withPassword("password")
+            }
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun properties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", container::getJdbcUrl)
+            registry.add("spring.datasource.username", container::getUsername)
+            registry.add("spring.datasource.password", container::getPassword)
+        }
+    }
+
+
+    @Test
+    fun applicationStarts() {
+        println(context.applicationName)
+    }
+}
