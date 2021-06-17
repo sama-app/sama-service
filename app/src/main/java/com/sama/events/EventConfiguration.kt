347f18f9fd01b7f27d3d489102551741e55575c1
balysv <balys@valentukevicius.com>
2021-06-18 00:23:07 +0200

    Add user deletion service

--- app/src/main/java/com/sama/events/EventConfiguration.kt
+++ app/src/main/java/com/sama/events/EventConfiguration.kt
@@ -3,6 +3,7 @@ package com.sama.events
 import com.google.common.eventbus.AsyncEventBus
 import com.sama.calendar.application.BlockEventConsumer
 import com.sama.events.guava.GuavaEventDispatcher
+import com.sama.meeting.application.MeetingEventConsumer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.util.concurrent.Executors
@@ -11,9 +12,10 @@ import java.util.concurrent.Executors
 class EventConfiguration {
 
     @Bean
-    fun eventDispatcher(listener: BlockEventConsumer): EventDispatcher {
+    fun eventDispatcher(listener: BlockEventConsumer, listener2: MeetingEventConsumer): EventDispatcher {
         val eventBus = AsyncEventBus("sama-async", Executors.newCachedThreadPool())
         eventBus.register(listener)
+        eventBus.register(listener2)
         return GuavaEventDispatcher(eventBus);
     }
 
