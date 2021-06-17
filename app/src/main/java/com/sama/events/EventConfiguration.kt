41aebdad89b0d4373c01e86a020f2e3c0ecfe9aa
balysv <balys@valentukevicius.com>
2021-06-18 00:32:46 +0200

    Remove dependency cycle

--- app/src/main/java/com/sama/events/EventConfiguration.kt
+++ app/src/main/java/com/sama/events/EventConfiguration.kt
@@ -1,6 +1,7 @@
 package com.sama.events
 
 import com.google.common.eventbus.AsyncEventBus
+import com.google.common.eventbus.EventBus
 import com.sama.calendar.application.BlockEventConsumer
 import com.sama.events.guava.GuavaEventDispatcher
 import com.sama.meeting.application.MeetingEventConsumer
@@ -12,15 +13,28 @@ import java.util.concurrent.Executors
 class EventConfiguration {
 
     @Bean
-    fun eventDispatcher(listener: BlockEventConsumer, listener2: MeetingEventConsumer): EventDispatcher {
-        val eventBus = AsyncEventBus("sama-async", Executors.newCachedThreadPool())
-        eventBus.register(listener)
-        eventBus.register(listener2)
-        return GuavaEventDispatcher(eventBus);
+    fun guavaEventBus(): EventBus {
+        return AsyncEventBus("sama-async", Executors.newCachedThreadPool());
+    }
+
+    @Bean
+    fun eventDispatcher(guavaEventBus: EventBus): EventDispatcher {
+        return GuavaEventDispatcher(guavaEventBus)
     }
 
     @Bean
     fun eventPublisher(eventDispatcher: EventDispatcher): EventPublisher {
         return TransactionalEventPublisher(eventDispatcher)
+    }
+
+    @Bean
+    fun eventRegistrations(
+        guavaEventBus: EventBus,
+        blockEventConsumer: BlockEventConsumer,
+        meetingEventConsumer: MeetingEventConsumer
+    ): Boolean {
+        guavaEventBus.register(blockEventConsumer)
+        guavaEventBus.register(meetingEventConsumer)
+        return true
     }
 }
