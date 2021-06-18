0aae4e5772135787fdac3f61d0caca228a2d3cb5
balysv <balys@valentukevicius.com>
2021-06-18 16:40:56 +0200

    Better way of adding event listeners

--- app/src/main/java/com/sama/meeting/application/MeetingEventConsumer.kt
+++ app/src/main/java/com/sama/meeting/application/MeetingEventConsumer.kt
@@ -3,12 +3,13 @@ package com.sama.meeting.application
 import com.google.common.eventbus.Subscribe
 import com.sama.calendar.application.BlockApplicationService
 import com.sama.calendar.application.CreateBlockCommand
+import com.sama.events.EventConsumer
 import com.sama.meeting.domain.MeetingConfirmedEvent
 import com.sama.users.domain.UserDeletedEvent
 import org.springframework.stereotype.Component
 
 @Component
-class MeetingEventConsumer(private val meetingApplicationService: MeetingApplicationService) {
+class MeetingEventConsumer(private val meetingApplicationService: MeetingApplicationService) : EventConsumer {
 
     @Subscribe
     fun onUserDeleted(event: UserDeletedEvent) {
