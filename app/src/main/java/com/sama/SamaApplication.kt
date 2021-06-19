722406f58ea4e7532a75b9b07435a158ed3f7eb7
balysv <balys@valentukevicius.com>
2021-06-19 13:14:26 +0200

    Add caching to historical heat map

--- app/src/main/java/com/sama/SamaApplication.kt
+++ app/src/main/java/com/sama/SamaApplication.kt
@@ -1,13 +1,21 @@
 package com.sama
 
+import com.github.benmanes.caffeine.cache.Caffeine
+import com.sama.slotsuggestion.domain.HistoricalHeapMap
+import com.sama.users.domain.UserId
+import org.checkerframework.checker.nullness.qual.NonNull
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.caffeine.CaffeineCacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import java.time.Clock
+import java.util.concurrent.TimeUnit
 
 
 @SpringBootApplication(
@@ -32,3 +40,7 @@ class AppConfiguration {
 @EnableJpaRepositories(basePackages = ["com.sama"])
 @EntityScan("com.sama")
 class PersistenceConfiguration
+
+@Configuration
+@EnableCaching
+class CacheConfiguration
---
+++ app/src/main/java/com/sama/slotsuggestion/application/FutureHeatMapService.kt
@@ -0,0 +1,40 @@
+package com.sama.slotsuggestion.application
+
+import com.sama.calendar.domain.BlockRepository
+import com.sama.common.ApplicationService
+import com.sama.common.findByIdOrThrow
+import com.sama.slotsuggestion.configuration.HeatMapConfiguration
+import com.sama.slotsuggestion.domain.*
+import com.sama.users.domain.UserId
+import com.sama.users.domain.UserSettingsRepository
+import org.springframework.stereotype.Service
+import java.time.LocalDate
+
+@ApplicationService
+@Service
+class FutureHeatMapService(
+    private val historicalHeatMapService: HistoricalHeatMapService,
+    private val userSettingsRepository: UserSettingsRepository,
+    private val blockRepository: BlockRepository,
+    private val heatMapConfiguration: HeatMapConfiguration,
+) {
+    fun find(userId: UserId): FutureHeatMap {
+        val historicalHeatMap = historicalHeatMapService.find(userId)
+
+        val userSettings = userSettingsRepository.findByIdOrThrow(userId)
+        val dayWorkingHours = userSettings.dayWorkingHours
+            .mapValues { it.value.workingHours }
+
+        val futureBlocks = blockRepository.findAll(
+            userId,
+            LocalDate.now().atStartOfDay(userSettings.timezone),
+            LocalDate.now().atStartOfDay(userSettings.timezone).plusDays(heatMapConfiguration.futureDays)
+        )
+            .map { Block(it.startDateTime, it.endDateTime) }
+            .groupBy { it.startDateTime.toLocalDate() }
+
+        return FutureHeatMapGenerator(
+            historicalHeatMap, dayWorkingHours, futureBlocks, heatMapConfiguration.futureDays
+        ).generate()
+    }
+}
