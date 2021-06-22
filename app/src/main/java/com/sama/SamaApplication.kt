73f1ed36a17b41a802f18d422e959957ec2278aa
balysv <balys@valentukevicius.com>
2021-06-22 18:33:23 +0200

    Add Sentry

--- app/src/main/java/com/sama/SamaApplication.kt
+++ app/src/main/java/com/sama/SamaApplication.kt
@@ -1,7 +1,6 @@
 package com.sama
 
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
@@ -9,7 +8,6 @@ import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-import org.springframework.transaction.annotation.EnableTransactionManagement
 import java.time.Clock
 
 
