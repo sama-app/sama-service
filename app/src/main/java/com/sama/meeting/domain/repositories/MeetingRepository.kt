737c4b879bc0bafc39429918dd319310cfefd8ef
balysv <balys@valentukevicius.com>
2021-07-04 11:52:07 +0200

    Mark meetings as expired when all proposed slots become in the past

--- app/src/main/java/com/sama/meeting/domain/repositories/MeetingRepository.kt
+++ app/src/main/java/com/sama/meeting/domain/repositories/MeetingRepository.kt
@@ -3,10 +3,14 @@ package com.sama.meeting.domain.repositories
 import com.sama.common.NotFoundException
 import com.sama.meeting.domain.MeetingCode
 import com.sama.meeting.domain.MeetingId
+import com.sama.meeting.domain.MeetingStatus
 import com.sama.meeting.domain.aggregates.MeetingEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
+import java.time.ZonedDateTime
 
 @Repository
 interface MeetingRepository : JpaRepository<MeetingEntity, MeetingId> {
@@ -14,6 +18,18 @@ interface MeetingRepository : JpaRepository<MeetingEntity, MeetingId> {
     fun nextIdentity(): MeetingId
 
     fun findByCode(code: MeetingCode): MeetingEntity?
+
+    @Query(
+        "SELECT m.id " +
+                "FROM MeetingEntity m JOIN MeetingProposedSlotEntity ps ON m.id = ps.meetingId " +
+                "WHERE m.status = 'PROPOSED' GROUP BY m.id " +
+                "HAVING max(ps.startDateTime) < :expiryDateTime"
+    )
+    fun findAllIdsExpiring(@Param("expiryDateTime") expiryDateTime: ZonedDateTime): Collection<MeetingId>
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE MeetingEntity m SET status = :status WHERE id IN :ids")
+    fun updateStatus(status: MeetingStatus, ids: Collection<MeetingId>)
 }
 
 fun MeetingRepository.findByCodeOrThrow(code: MeetingCode): MeetingEntity = findByCode(code)
