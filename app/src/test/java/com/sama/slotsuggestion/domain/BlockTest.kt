58824c6cfe240ca0008d7ffe104d065bd9ef4cc1
balysv <balys@valentukevicius.com>
2021-07-20 13:21:00 +0200

    Handle zero duration meetings and add contextual logging
    Exception handling to be removed

---
+++ app/src/test/java/com/sama/slotsuggestion/domain/BlockTest.kt
@@ -0,0 +1,19 @@
+package com.sama.slotsuggestion.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.time.ZonedDateTime
+
+
+internal class BlockTest {
+
+    @Test
+    fun testZeroDuration() {
+        val now = ZonedDateTime.now()
+        assertThat(Block(now, now, false, false, 0, null).zeroDuration())
+            .isTrue()
+
+        assertThat(Block(now, now.plusMinutes(1), false, false, 0, null).zeroDuration())
+            .isFalse()
+    }
+}
