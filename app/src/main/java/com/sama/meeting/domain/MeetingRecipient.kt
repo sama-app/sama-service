2dd816b6605f5cea07883943656e58380bbff8df
balysv <balys@valentukevicius.com>
2021-08-02 13:25:11 +0300

    Properly validate user and recipient e-mails

--- app/src/main/java/com/sama/meeting/domain/MeetingRecipient.kt
+++ app/src/main/java/com/sama/meeting/domain/MeetingRecipient.kt
@@ -2,7 +2,9 @@ package com.sama.meeting.domain
 
 import com.sama.common.Factory
 import com.sama.common.ValueObject
+import com.sama.users.domain.InvalidEmailException
 import com.sama.users.domain.UserId
+import org.apache.commons.validator.routines.EmailValidator
 import javax.persistence.Embeddable
 
 @Embeddable
@@ -11,6 +13,10 @@ data class MeetingRecipient(val recipientId: UserId?, val email: String?) {
     @Factory
     companion object {
         fun fromEmail(email: String): MeetingRecipient {
+            val isEmailValid = EmailValidator.getInstance().isValid(email)
+            if (!isEmailValid) {
+                throw InvalidEmailException(email)
+            }
             return MeetingRecipient(null, email)
         }
     }
