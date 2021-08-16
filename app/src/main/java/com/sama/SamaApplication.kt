94e5780b398c25f356184d6b8135462d501b7a91
balysv <balys@valentukevicius.com>
2021-08-16 16:43:23 +0200

    Enable HTTP2

--- app/src/main/java/com/sama/SamaApplication.kt
+++ app/src/main/java/com/sama/SamaApplication.kt
@@ -1,22 +1,19 @@
 package com.sama
 
+import io.undertow.UndertowOptions.ENABLE_HTTP2
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.web.embedded.undertow.UndertowBuilderCustomizer
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
-import org.springframework.jdbc.datasource.DataSourceTransactionManager
 import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.transaction.TransactionManager
 import java.time.Clock
-import javax.sql.DataSource
 
 
 @SpringBootApplication(
@@ -35,6 +32,16 @@ class AppConfiguration {
     @Bean
     fun clock(): Clock {
         return Clock.systemUTC()
+    }
+
+    @Bean
+    fun undertowServletWebServerFactory(): UndertowServletWebServerFactory {
+        val factory = UndertowServletWebServerFactory()
+        factory.addBuilderCustomizers(
+            UndertowBuilderCustomizer {
+                it.setServerOption(ENABLE_HTTP2, true)
+            })
+        return factory
     }
 }
 
