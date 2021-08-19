6692e82b69d64ede19c966d0152cac0e219b122a
balysv <balys@valentukevicius.com>
2021-08-19 11:48:04 +0200

    Store google tokens encrypted

--- app/src/main/java/com/sama/users/domain/UserGoogleCredential.kt
+++ app/src/main/java/com/sama/users/domain/UserGoogleCredential.kt
@@ -5,6 +5,7 @@ import com.sama.common.ValueObject
 import java.time.Instant
 import javax.persistence.Column
 import javax.persistence.Embeddable
+import org.springframework.security.crypto.encrypt.TextEncryptor
 
 @DomainEntity
 data class UserGoogleCredential(
@@ -18,7 +19,7 @@ data class UserGoogleCredential(
  */
 @ValueObject
 @Embeddable
-data class GoogleCredential(
+data class GoogleCredential private constructor (
 
     @Column(name = "google_access_token", table = "user_google_credential")
     val accessToken: String?,
@@ -26,16 +27,54 @@ data class GoogleCredential(
     @Column(name = "google_refresh_token", table = "user_google_credential")
     val refreshToken: String?,
 
+    @Column(name = "google_access_token_encrypted", table = "user_google_credential")
+    val accessTokenEncrypted: String? = null,
+
+    @Column(name = "google_refresh_token_encrypted", table = "user_google_credential")
+    val refreshTokenEncrypted: String? = null,
+
     @Column(name = "google_token_expiration_time_ms", table = "user_google_credential")
     val expirationTimeMs: Long?,
 
     @Column(name = "updated_at", table = "user_google_credential")
     var updatedAt: Instant? = null
 ) {
+    companion object {
+        fun plainText(accessToken: String?, refreshToken: String?, expirationTimeMs: Long?): GoogleCredential {
+            return GoogleCredential(accessToken, refreshToken, expirationTimeMs = expirationTimeMs)
+        }
+
+        fun encrypted(
+            accessToken: String?,
+            refreshToken: String?,
+            expirationTimeMs: Long?,
+            encryptor: TextEncryptor
+        ): GoogleCredential {
+            return GoogleCredential(
+                accessToken,
+                refreshToken,
+                accessToken?.let { encryptor.encrypt(it) },
+                refreshToken?.let { encryptor.encrypt(it) },
+                expirationTimeMs
+            )
+        }
+    }
+
+    fun encrypt(encryptor: TextEncryptor): GoogleCredential {
+        return copy(
+            accessTokenEncrypted = accessToken?.let { encryptor.encrypt(it) },
+            refreshTokenEncrypted = refreshToken?.let { encryptor.encrypt(it) },
+        )
+    }
+
 
     fun merge(newCredential: GoogleCredential): GoogleCredential {
-        return if (newCredential.refreshToken == null) {
-            newCredential.copy(refreshToken = refreshToken, updatedAt = Instant.now())
+        return if (newCredential.refreshToken == null || newCredential.refreshTokenEncrypted == null) {
+            newCredential.copy(
+                refreshToken = refreshToken,
+                refreshTokenEncrypted = refreshTokenEncrypted,
+                updatedAt = Instant.now()
+            )
         } else {
             newCredential.copy(updatedAt = Instant.now())
         }
