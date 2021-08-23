4c19d721ee04e1a2a1af2496d178b00ee1bb9798
balysv <balys@valentukevicius.com>
2021-08-23 14:55:48 +0200

    Algorithm tweaks
    - Created "thisOrNextWeek" template that is the default
    - Optimized weighers to not iterate on each block

--- app/src/main/java/com/sama/meeting/application/MeetingApplicationService.kt
+++ app/src/main/java/com/sama/meeting/application/MeetingApplicationService.kt
@@ -28,7 +28,6 @@ import java.time.Clock
 import java.time.ZonedDateTime
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.security.access.AccessDeniedException
-import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
--- app/src/main/java/com/sama/slotsuggestion/application/HeatMapServiceV2.kt
+++ app/src/main/java/com/sama/slotsuggestion/application/HeatMapServiceV2.kt
@@ -2,12 +2,14 @@ package com.sama.slotsuggestion.application
 
 import com.sama.common.ApplicationService
 import com.sama.integration.sentry.sentrySpan
+import com.sama.meeting.application.MeetingDataService
 import com.sama.slotsuggestion.configuration.HeatMapConfiguration
 import com.sama.slotsuggestion.domain.BlockRepository
 import com.sama.slotsuggestion.domain.UserRepository
 import com.sama.slotsuggestion.domain.v2.HeatMap
 import com.sama.slotsuggestion.domain.v2.weigher
 import com.sama.users.domain.UserId
+import java.time.Clock
 import java.time.LocalDate
 import java.time.ZoneId
 import org.springframework.stereotype.Service
@@ -17,17 +19,21 @@ import org.springframework.stereotype.Service
 class HeatMapServiceV2(
     private val userRepository: UserRepository,
     private val blockRepository: BlockRepository,
+    private val meetingDataService: MeetingDataService,
     private val heatMapConfiguration: HeatMapConfiguration,
+    private val clock: Clock
 ) {
     fun generate(initiatorId: UserId, recipientTimezone: ZoneId): HeatMap {
         val user = userRepository.findById(initiatorId)
-        val today = LocalDate.now(user.timeZone).atStartOfDay(user.timeZone)
+        val today = LocalDate.now(clock.withZone(user.timeZone)).atStartOfDay(user.timeZone)
 
         val pastBlockStartDate = today.minusDays(heatMapConfiguration.historicalDays)
         val pastBlocks = blockRepository.findAllBlocksCached(initiatorId, pastBlockStartDate, today)
 
         val futureBlockEndDate = today.plusDays(heatMapConfiguration.futureDays)
         val futureBlocks = blockRepository.findAllBlocks(initiatorId, today, futureBlockEndDate)
+
+        val futureProposedSlots = meetingDataService.findProposedSlots(initiatorId, today, futureBlockEndDate)
 
         sentrySpan(method = "HeatMap.create") {
             val baseHeatMap = HeatMap.create(
@@ -42,7 +48,7 @@ class HeatMapServiceV2(
                 searchBoundary()
                 pastBlocks(pastBlocks)
                 futureBlocks(futureBlocks)
-//                 futureProposedSlots(futureProposedSlots)
+                futureProposedSlots(futureProposedSlots)
                 workingHours(user.workingHours)
                 recipientTimeZone(recipientTimezone)
                 recency()
