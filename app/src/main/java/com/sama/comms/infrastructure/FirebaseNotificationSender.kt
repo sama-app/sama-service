6fa33f8b563185543557d4089d8628df0b8d7f3a
balysv <balys@valentukevicius.com>
2021-09-30 18:17:07 +0200

    Unregister firebase device if failed to send

--- app/src/main/java/com/sama/comms/infrastructure/FirebaseNotificationSender.kt
+++ app/src/main/java/com/sama/comms/infrastructure/FirebaseNotificationSender.kt
@@ -3,8 +3,10 @@ package com.sama.comms.infrastructure
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingException
 import com.google.firebase.messaging.Message
+import com.google.firebase.messaging.MessagingErrorCode
 import com.sama.comms.domain.Notification
 import com.sama.comms.domain.NotificationSender
+import com.sama.users.application.UnregisterDeviceCommand
 import com.sama.users.application.UserService
 import com.sama.users.domain.UserId
 import org.slf4j.Logger
@@ -16,7 +18,6 @@ class FirebaseNotificationSender(private val userService: UserService) : Notific
     private var logger: Logger = LoggerFactory.getLogger(FirebaseNotificationSender::class.java)
 
     override fun send(userId: UserId, notification: Notification) {
-
         val response = userService.findUserDeviceRegistrations(userId)
         if (response.firebaseDeviceRegistrations.isEmpty()) {
             throw RuntimeException("No Firebase device registrations found")
@@ -37,6 +38,14 @@ class FirebaseNotificationSender(private val userService: UserService) : Notific
                 FirebaseMessaging.getInstance().send(message)
                 logger.debug("Notifications sent to User#${userId.id}  Device#${it.deviceId}")
             } catch (e: Exception) {
+                if (e is FirebaseMessagingException) {
+                    // Remove unregistered devices
+                    if (e.messagingErrorCode == MessagingErrorCode.UNREGISTERED) {
+                        kotlin.runCatching {
+                            userService.unregisterDevice(userId, UnregisterDeviceCommand(it.deviceId))
+                        }
+                    }
+                }
                 val code = if (e is FirebaseMessagingException) e.messagingErrorCode.toString() else "OTHER"
                 logger.warn("Could not send Firebase notification to User#${userId.id} Device#${it.deviceId}: $code", e)
             }
--- app/src/main/java/com/sama/users/application/UserService.kt
+++ app/src/main/java/com/sama/users/application/UserService.kt
@@ -6,4 +6,5 @@ interface UserService {
     fun find(userId: UserId): UserPublicDTO
     fun findAll(userIds: Collection<UserId>): Map<UserId, UserPublicDTO>
     fun findUserDeviceRegistrations(userId: UserId): UserDeviceRegistrationsDTO
+    fun unregisterDevice(userId: UserId, command: UnregisterDeviceCommand): Boolean
 }
