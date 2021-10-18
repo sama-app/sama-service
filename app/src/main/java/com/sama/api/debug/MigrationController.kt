fe23b53a5b9350f3f67087db0bbbda6969b88e02
balysv <balys@valentukevicius.com>
2021-10-18 21:40:14 +0200

    Perform calendar sync migration in the background

--- app/src/main/java/com/sama/api/debug/MigrationController.kt
+++ app/src/main/java/com/sama/api/debug/MigrationController.kt
@@ -4,8 +4,10 @@ import com.sama.integration.google.auth.domain.GoogleAccountRepository
 import com.sama.integration.google.calendar.application.GoogleCalendarSyncer
 import com.sama.integration.google.calendar.domain.CalendarListRepository
 import io.swagger.v3.oas.annotations.Hidden
+import java.time.Instant
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.scheduling.TaskScheduler
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -14,27 +16,42 @@ import org.springframework.web.bind.annotation.RestController
 class MigrationController(
     private val googleAccountRepository: GoogleAccountRepository,
     private val calendarListRepository: CalendarListRepository,
-    private val googleCalendarSyncer: GoogleCalendarSyncer
+    private val googleCalendarSyncer: GoogleCalendarSyncer,
+    private val taskScheduler: TaskScheduler
 ) {
     private var logger: Logger = LoggerFactory.getLogger(MigrationController::class.java)
 
     @PostMapping("/api/__migration/001-migrate-calendar-sync")
-    fun migrateCalendarSync() {
-        val accountIds = googleAccountRepository.findAllIds()
-        accountIds.forEach { accountId ->
-            kotlin.runCatching { googleCalendarSyncer.enableCalendarListSync(accountId) }
-                .onSuccess { logger.info("Migrated CalendarList for GoogleAccount#${accountId.id}") }
-                .onFailure { logger.warn("Failed to migrate CalendarList for GoogleAccount$accountId: ${it.message}", it) }
-
-            calendarListRepository.find(accountId)
-                ?.syncableCalendars
-                ?.forEach { calendarId ->
-                    kotlin.runCatching { googleCalendarSyncer.enableCalendarSync(accountId, calendarId) }
-                        .onSuccess { logger.info("Migrated Calendar#$calendarId for GoogleAccount#${accountId.id}") }
+    fun migrateCalendarSync(): String {
+        taskScheduler.schedule(
+            {
+                val accountIds = googleAccountRepository.findAllIds()
+                accountIds.forEach { accountId ->
+                    kotlin.runCatching { googleCalendarSyncer.enableCalendarListSync(accountId) }
+                        .onSuccess { logger.info("Migrated CalendarList for GoogleAccount#${accountId.id}") }
                         .onFailure {
-                            logger.warn("Failed to migrate Calendar#$calendarId for GoogleAccount$accountId: ${it.message}", it)
+                            logger.warn(
+                                "Failed to migrate CalendarList for GoogleAccount$accountId: ${it.message}",
+                                it
+                            )
+                        }
+
+                    calendarListRepository.find(accountId)
+                        ?.syncableCalendars
+                        ?.forEach { calendarId ->
+                            kotlin.runCatching { googleCalendarSyncer.enableCalendarSync(accountId, calendarId) }
+                                .onSuccess { logger.info("Migrated Calendar#$calendarId for GoogleAccount#${accountId.id}") }
+                                .onFailure {
+                                    logger.warn(
+                                        "Failed to migrate Calendar#$calendarId for GoogleAccount$accountId: ${it.message}",
+                                        it
+                                    )
+                                }
                         }
                 }
-        }
+            },
+            Instant.now()
+        )
+        return "OK"
     }
 }
