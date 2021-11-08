8620ae6b5e055bc7f7f77bdf8ca91e4adc74dc08
balysv <balys@valentukevicius.com>
2021-11-08 09:18:39 +0100

    Return 401 is using an expired JWT even on non-auth endpoints

--- app/src/main/java/com/sama/api/config/security/JwtAuthorizationFilter.kt
+++ app/src/main/java/com/sama/api/config/security/JwtAuthorizationFilter.kt
@@ -1,18 +1,19 @@
 package com.sama.api.config.security
 
+import com.auth0.jwt.exceptions.TokenExpiredException
 import com.sama.users.domain.Jwt
 import com.sama.users.domain.JwtConfiguration
 import com.sama.users.domain.UserPublicId
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.util.StringUtils
-import org.springframework.web.filter.OncePerRequestFilter
 import java.io.IOException
 import java.time.Clock
 import javax.servlet.FilterChain
 import javax.servlet.ServletException
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.util.StringUtils
+import org.springframework.web.filter.OncePerRequestFilter
 
 
 /**
@@ -33,7 +34,19 @@ class JwtAuthorizationFilter(
                     val auth = UsernamePasswordAuthenticationToken(userPrincipal, null, null)
                     SecurityContextHolder.getContext().authentication = auth
                 }
-                .onFailure { logger.info("jwt authentication error:", it) }
+                .onFailure {
+                    if (it is TokenExpiredException) {
+                        // Set Principal if token is valid but expired so we can act on it
+                        Jwt.raw(token).getOrNull()
+                            ?.let { jwt ->
+                                val userPrincipal = UserPrincipal(jwt.userEmail(), jwt.userId())
+                                // this means NOT authenticated
+                                val auth = UsernamePasswordAuthenticationToken(userPrincipal, null)
+                                SecurityContextHolder.getContext().authentication = auth
+                            }
+                    }
+                    logger.debug("jwt authentication error:", it)
+                }
         }
 
         chain.doFilter(request, response)
