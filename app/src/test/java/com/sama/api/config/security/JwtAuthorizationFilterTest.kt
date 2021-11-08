4a3eea6a2f6f7de466158ea0deebdcd8d031f9ba
balysv <balys@valentukevicius.com>
2021-11-08 09:18:39 +0100

    Return 401 is using an expired JWT even on non-auth endpoints

--- app/src/test/java/com/sama/api/config/security/JwtAuthorizationFilterTest.kt
+++ app/src/test/java/com/sama/api/config/security/JwtAuthorizationFilterTest.kt
@@ -3,10 +3,14 @@ package com.sama.api.config.security
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import com.sama.api.ApiTestConfiguration
+import com.sama.api.config.WebMvcConfiguration
 import com.sama.api.jwtKeyId
 import com.sama.api.jwtSigningSecret
-import com.sama.api.config.WebMvcConfiguration
 import com.sama.users.domain.USER_ID_CLAIM
+import java.time.Clock
+import java.util.Date
+import java.util.UUID
+import javax.servlet.http.Cookie
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -18,11 +22,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import java.time.Clock
-import java.time.Instant
-import java.time.ZoneId
-import java.util.*
-import javax.servlet.http.Cookie
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(
@@ -57,7 +56,7 @@ internal class AuthorizationFiltersTest(
     @Test
     fun `jwt authorization works`() {
         mockMvc.perform(
-            get("/test-endpoint")
+            get("/api/test-endpoint")
                 .header("Authorization", "Bearer $validJwt")
         )
             .andExpect(MockMvcResultMatchers.status().isOk)
@@ -65,14 +64,14 @@ internal class AuthorizationFiltersTest(
 
     @Test
     fun `missing access token returns 401`() {
-        mockMvc.perform(get("/test-endpoint"))
+        mockMvc.perform(get("/api/test-endpoint"))
             .andExpect(MockMvcResultMatchers.status().isUnauthorized)
     }
 
     @Test
     fun `authorization with invalid header returns 401`() {
         mockMvc.perform(
-            get("/test-endpoint")
+            get("/api/test-endpoint")
                 .header("Authorization", "Invalid $validJwt")
         )
             .andExpect(MockMvcResultMatchers.status().isUnauthorized)
@@ -81,7 +80,7 @@ internal class AuthorizationFiltersTest(
     @Test
     fun `authorization with expired jwt returns 401`() {
         mockMvc.perform(
-            get("/test-endpoint")
+            get("/api/test-endpoint")
                 .header("Authorization", "Bearer $expiredJwt")
         )
             .andExpect(MockMvcResultMatchers.status().isUnauthorized)
@@ -90,8 +89,8 @@ internal class AuthorizationFiltersTest(
     @Test
     fun `authorization with cookie jwt works`() {
         mockMvc.perform(
-            get("/test-endpoint")
-                .cookie(Cookie( "sama.access", validJwt))
+            get("/api/test-endpoint")
+                .cookie(Cookie("sama.access", validJwt))
         )
             .andExpect(MockMvcResultMatchers.status().isOk)
     }
@@ -99,8 +98,35 @@ internal class AuthorizationFiltersTest(
     @Test
     fun `authorization with expired cookie jwt returns 401`() {
         mockMvc.perform(
-            get("/test-endpoint")
-                .cookie(Cookie( "sama.access", expiredJwt))
+            get("/api/test-endpoint")
+                .cookie(Cookie("sama.access", expiredJwt))
+        )
+            .andExpect(MockMvcResultMatchers.status().isUnauthorized)
+    }
+
+    @Test
+    fun `no jwt for no-auth endpoint returns 200`() {
+        mockMvc.perform(
+            get("/api/auth/no-auth")
+                .header("Authorization", "Bearer $validJwt")
+        )
+            .andExpect(MockMvcResultMatchers.status().isOk)
+    }
+
+    @Test
+    fun `invalid auth header for no-auth endpoint returns 200`() {
+        mockMvc.perform(
+            get("/api/auth/no-auth")
+                .header("Authorization", "Bearer GIBBERISH")
+        )
+            .andExpect(MockMvcResultMatchers.status().isOk)
+    }
+
+    @Test
+    fun `expired jwt for no-auth endpoint returns 401`() {
+        mockMvc.perform(
+            get("/api/auth/no-auth")
+                .header("Authorization", "Bearer $expiredJwt")
         )
             .andExpect(MockMvcResultMatchers.status().isUnauthorized)
     }
@@ -109,6 +135,10 @@ internal class AuthorizationFiltersTest(
 @RestController
 class AuthTestController {
 
-    @GetMapping("test-endpoint")
+    @GetMapping("/api/test-endpoint")
     fun testEndpoint() = "alohomora!"
+
+
+    @GetMapping("/api/auth/no-auth")
+    fun unauthenticatedEndpoint() = "alohomora!"
 }
