ef3b07a2d625aad19cf7daaa7baa8778d5d55dae
balysv <balys@valentukevicius.com>
2021-11-10 12:30:48 +0100

    HeatMap overlay experiment for Tomas + Kei

--- app/src/main/java/com/sama/slotsuggestion/application/DefaultSlotSuggestionService.kt
+++ app/src/main/java/com/sama/slotsuggestion/application/DefaultSlotSuggestionService.kt
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Service
 
 @Service
-@Primary
 class DefaultSlotSuggestionService(
     private val heatMapSlotSuggestionService: HeatMapSlotSuggestionService,
 ) : SlotSuggestionService {
