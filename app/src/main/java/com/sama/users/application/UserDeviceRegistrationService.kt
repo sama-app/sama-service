d34515ddcb756e0c4979d59b75616ee42475d605
balysv <balys@valentukevicius.com>
2021-11-17 09:11:49 +0100

    Fix user device unregistration in case of failed PNs

--- app/src/main/java/com/sama/users/application/UserDeviceRegistrationService.kt
+++ app/src/main/java/com/sama/users/application/UserDeviceRegistrationService.kt
@@ -1,10 +1,13 @@
 package com.sama.users.application
 
+import com.sama.common.InternalApi
 import com.sama.users.domain.UserId
 
 interface UserDeviceRegistrationService {
     fun me(): UserDeviceRegistrationsDTO
     fun find(userId: UserId): UserDeviceRegistrationsDTO
     fun register(command: RegisterDeviceCommand): Boolean
+    @InternalApi
+    fun unregister(userId: UserId, command: UnregisterDeviceCommand): Boolean
     fun unregister(command: UnregisterDeviceCommand): Boolean
 }
